a51e191a943634cb30888e8429d2574beecb57fd
helmyandria <helmyandria@gmail.com>
2023-08-24 16:53:22 +0700

    add create scope

--- src/main/java/programmerzamannow/spring/core/ScopeConfiguration.java
+++ src/main/java/programmerzamannow/spring/core/ScopeConfiguration.java
@@ -1,11 +1,15 @@
 package programmerzamannow.spring.core;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomEditorConfigurer;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
+import programmerzamannow.spring.core.data.Bar;
 import programmerzamannow.spring.core.data.Foo;
+import scope.DoubletonScope;
 
 @Slf4j
 @Configuration
@@ -16,6 +20,20 @@ public class ScopeConfiguration {
     public Foo foo(){
         log.info("Create new Foo");
         return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer(){
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar(){
+        log.info("Create new Bar");
+        return new Bar();
     }
 
 }
