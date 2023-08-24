411d1a902c5231d4df1230f029512dd63119a95f
helmyandria <helmyandria@gmail.com>
2023-08-24 09:56:23 +0700

    add singleton

---
+++ src/main/java/programmerzamannow/spring/core/Database.java
@@ -0,0 +1,17 @@
+package programmerzamannow.spring.core;
+
+public class Database {
+
+    private  static Database database;
+
+    public static Database getInstance(){
+        if(database == null){
+            database = new Database();
+        }
+        return database;
+    }
+
+    private Database(){
+
+    }
+}
