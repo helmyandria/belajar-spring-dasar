62f5ccb109a124e5ae58d4c75b50e9fb7bf4925f
helmyandria <helmyandria@gmail.com>
2023-08-24 12:27:01 +0700

    add dependency injection

---
+++ src/main/java/programmerzamannow/spring/core/DependencyInjectionConfiguration.java
@@ -0,0 +1,24 @@
+package programmerzamannow.spring.core;
+
+import org.springframework.context.annotation.Bean;
+import programmerzamannow.spring.core.data.Bar;
+import programmerzamannow.spring.core.data.Foo;
+import programmerzamannow.spring.core.data.FooBar;
+
+public class DependencyInjectionConfiguration {
+
+    @Bean
+    public Foo foo(){
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar(){
+        return new Bar();
+    }
+
+    @Bean
+    public FooBar fooBar(Foo foo, Bar bar){
+        return new FooBar(foo, bar);
+    }
+}
