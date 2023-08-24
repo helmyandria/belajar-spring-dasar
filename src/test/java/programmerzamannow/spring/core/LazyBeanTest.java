e00dcf51a12efd4f4a3bdc927ed5ba1d4acd6d18
helmyandria <helmyandria@gmail.com>
2023-08-24 14:36:55 +0700

    add lazy bean

---
+++ src/test/java/programmerzamannow/spring/core/LazyBeanTest.java
@@ -0,0 +1,21 @@
+package programmerzamannow.spring.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import programmerzamannow.spring.core.data.Foo;
+
+public class LazyBeanTest {
+    
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(LazyBeanConfiguration.class);
+    }
+
+    @Test
+    void testDependsOn() {
+    }
+}
