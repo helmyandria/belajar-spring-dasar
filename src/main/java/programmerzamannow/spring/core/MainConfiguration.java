51f9bec77877599bce448bac9158d66f090ed6c2
helmyandria <helmyandria@gmail.com>
2023-08-25 10:18:55 +0700

    add component scan

--- src/main/java/programmerzamannow/spring/core/MainConfiguration.java
+++ src/main/java/programmerzamannow/spring/core/MainConfiguration.java
@@ -2,6 +2,8 @@ package programmerzamannow.spring.core;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import programmerzamannow.spring.core.configuration.BarConfiguration;
+import programmerzamannow.spring.core.configuration.FooConfiguration;
 
 @Configuration
 @Import({
--- src/main/java/programmerzamannow/spring/core/configuration/FooConfiguration.java
+++ src/main/java/programmerzamannow/spring/core/configuration/FooConfiguration.java
@@ -1,4 +1,4 @@
-package programmerzamannow.spring.core;
+package programmerzamannow.spring.core.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
