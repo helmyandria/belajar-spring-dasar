d81c2c85c8eed1ddbf483ac4d7cc39febe85e6f9
helmyandria <helmyandria@gmail.com>
2023-08-25 10:59:38 +0700

    add constructor based dependecy injection

--- src/test/java/programmerzamannow/spring/core/ComponentTest.java
+++ src/test/java/programmerzamannow/spring/core/ComponentTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import programmerzamannow.spring.core.repository.ProductRepository;
 import programmerzamannow.spring.core.service.ProductService;
 
 public class ComponentTest {
@@ -23,5 +24,14 @@ public class ComponentTest {
         ProductService productService2 = applicationContext.getBean("productService",ProductService.class);
 
         Assertions.assertSame(productService1, productService2);
+    }
+
+    @Test
+    void testConstructorDependencyInjection(){
+        ProductService productService = applicationContext.getBean(ProductService.class);
+
+        ProductRepository productRepository = applicationContext.getBean(ProductRepository.class);
+
+        Assertions.assertSame(productRepository, productService.getProductRepository());
     }
 }
