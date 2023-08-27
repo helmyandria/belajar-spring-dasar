c212b6ac87c375d5f7a05fbf80ffd3e6e002b7b7
helmyandria <helmyandria@gmail.com>
2023-08-27 11:55:59 +0700

    add optional dependency

--- src/main/java/programmerzamannow/spring/core/ComponentConfiguration.java
+++ src/main/java/programmerzamannow/spring/core/ComponentConfiguration.java
@@ -2,6 +2,8 @@ package programmerzamannow.spring.core;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import programmerzamannow.spring.core.data.MultiFoo;
 
 @Configuration
 @ComponentScan(basePackages = {
@@ -9,5 +11,6 @@ import org.springframework.context.annotation.Configuration;
         "programmerzamannow.spring.core.repository",
         "programmerzamannow.spring.core.configuration",
 })
+@Import(MultiFoo.class)
 public class ComponentConfiguration {
 }
