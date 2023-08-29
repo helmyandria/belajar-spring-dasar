3d241df4424f0b20ea27cb58311226c481a2f6bc
helmyandria <helmyandria@gmail.com>
2023-08-29 11:21:22 +0700

    add event listener

---
+++ src/main/java/programmerzamannow/spring/core/listener/LoginSuccessListener.java
@@ -0,0 +1,17 @@
+package programmerzamannow.spring.core.listener;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import programmerzamannow.spring.core.event.LoginSuccessEvent;
+
+@Component
+@Slf4j
+public class LoginSuccessListener implements ApplicationListener<LoginSuccessEvent> {
+
+    @Override
+    public void onApplicationEvent(LoginSuccessEvent event) {
+        log.info("Success login for user {}", event.getUser());
+    }
+}
