89c081bff0715b565c0bc2dbffee6327bb005d07
helmyandria <helmyandria@gmail.com>
2023-08-29 13:25:11 +0700

    add event listener annotation

--- src/test/java/programmerzamannow/spring/core/EventListenerTest.java
+++ src/test/java/programmerzamannow/spring/core/EventListenerTest.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import programmerzamannow.spring.core.listener.LoginAgainSuccessListener;
 import programmerzamannow.spring.core.listener.LoginSuccessListener;
+import programmerzamannow.spring.core.listener.UserListener;
 import programmerzamannow.spring.core.service.UserService;
 
 public class EventListenerTest {
@@ -16,7 +17,8 @@ public class EventListenerTest {
     @Import({
             UserService.class,
             LoginSuccessListener.class,
-            LoginAgainSuccessListener.class
+            LoginAgainSuccessListener.class,
+            UserListener.class
     })
     public static class TestConfiguration {
 
