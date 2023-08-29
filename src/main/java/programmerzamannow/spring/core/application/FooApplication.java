540e7a28cc36eba0c309e587625d8aab9dba540c
helmyandria <helmyandria@gmail.com>
2023-08-29 13:39:14 +0700

    add spring boot application

---
+++ src/main/java/programmerzamannow/spring/core/application/FooApplication.java
@@ -0,0 +1,24 @@
+package programmerzamannow.spring.core.application;
+
+import lombok.val;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import programmerzamannow.spring.core.data.Foo;
+
+@SpringBootApplication
+public class FooApplication {
+
+    @Bean
+    public Foo foo(){
+        return new Foo();
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+
+        Foo foo = applicationContext.getBean(Foo.class);
+        System.out.println(foo );
+    }
+}
