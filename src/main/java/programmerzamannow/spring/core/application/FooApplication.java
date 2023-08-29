10100f18fbbf4a0ee1342fc9bccc92802ff0590d
helmyandria <helmyandria@gmail.com>
2023-08-29 14:29:53 +0700

    add customizing spring application

--- src/main/java/programmerzamannow/spring/core/application/FooApplication.java
+++ src/main/java/programmerzamannow/spring/core/application/FooApplication.java
@@ -1,6 +1,7 @@
 package programmerzamannow.spring.core.application;
 
 import lombok.val;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -20,10 +21,24 @@ public class FooApplication {
         return new Foo();
     }
 
+//    public static void main(String[] args) {
+//        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+//
+//        Foo foo = applicationContext.getBean(Foo.class);
+//        System.out.println(foo );
+//    }
+
+
+    /**
+     * Customizing Spring Application
+     */
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+        SpringApplication application = new SpringApplication(FooApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+
+        ConfigurableApplicationContext applicationContext = application.run(args);
 
         Foo foo = applicationContext.getBean(Foo.class);
-        System.out.println(foo );
+        System.out.println(foo);
     }
 }
